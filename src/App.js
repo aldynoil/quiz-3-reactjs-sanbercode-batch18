13c17f2d2afa21e1f132c9ce566ad725b484a654
aldynoil <aldynomilham25@gmail.com>
2020-10-18 20:32:00 +0700

    Quiz 3

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import Home from './Component/home';
+import About from './Component/about';
+
+function App() {
+  return (
+    <div>
+    <Home/>
+    <About/>
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ src/Component/about.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="sections">
+      <h1 className="text-center title">
+        Data Peserta Sanbercode Bootcamp Reactjs
+      </h1>
+      <div className="container">
+        <ol>
+          <li>
+            <b>Nama:</b> Ilham Aldyno
+          </li>
+          <li>
+            <b>Email:</b> aldynomilham25@gmail.com
+          </li>
+          <li>
+            <b>Sistem Operasi yang digunakan:</b> Windows 10
+          </li>
+          <li>
+            <b>Akun Github:</b> aldynoil
+          </li>
+          <li>
+            <b>Akun Telegram:</b> ilham aldyno
+          </li>
+        </ol>
+      </div>
+    </section>
+  );
+};
+
+export default About;
